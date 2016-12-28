1c7e51cb02b428862ab0f436db46d8082605c34b
wzgdavid <wzgdavid@sina.com>
2016-12-28 22:53:05 +0800

    general index

--- restart/general_index.py
+++ restart/general_index.py
@@ -1,9 +1,10 @@
 # encoding: utf-8
-import sys
-sys.path.append("..")
+#import sys
+#sys.path.append("..")
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 class General(object):
     def __init__(self, daima):
@@ -19,20 +20,70 @@ class General(object):
 class GeneralIndex(General):
     def __init__(self, daima):
         super(GeneralIndex, self).__init__(daima)
-        #self.sdjj()
+        self.get_sdjj()
     #def __new__(self, daima):
     #    super(General, self).__init__(daima)
 
     #def ma5(self, n):
     #    self.ma5 = 
 
-    def sdjj(self):
+    def get_sdjj(self):
+        '''
+        四点均价  开收盘价，最高价，最低价
+        '''
         self.df['sdjj'] = (self.df['o'] + self.df['c'] + self.df['h'] + self.df['l']) / 4
+
+    #def get_ma5(self):
+        #df['ma5'] = pd.rolling(self.df.c.shift(-1*5), 5).mean()
+        
+    #    self.df['ma5'] = self.df.c.rolling(window=5,center=False).mean()
+
+    def get_ma(self, *malist):
+        '''get_ma(5,10,20)  得到5,10,20周期的ma
+        '''
+        for n in malist:
+            self.df['ma%s' % n]  = self.df.c.rolling(window=n, center=False).mean()
+
+    def get_high_percent(self, n):
+        '''最高点向下百分比
+        '''
+        self.df['hp'] = self.df.h * (1 - float(n)/100)
+
+    def get_low_percent(self, n):
+        '''最低点向上百分比
+        '''
+        self.df['lp'] = self.df.l * (1 + float(n)/100)
+
+    def get_nhh(self, n):
+        '''前n天最高价最高点（不包含当天）'''
+        self.df['nhh'] = self.df.h.shift(1).rolling(window=n, center=False).max()
+
+    def get_nll(self, n):
+        '''前n天最低价最低点（不包含当天）'''
+        self.df['nll'] = self.df.l.shift(1).rolling(window=n, center=False).min()
+
+    def get_nsdh(self, n):
+        '''前n天四点均价最高点（不包含当天）'''
+        self.df['nsdh'] = self.df.sdjj.shift(1).rolling(window=n, center=False).max()
+
+    def get_nsdl(self, n):
+        '''前n天四点均价最低点（不包含当天）'''
+        self.df['nsdl'] = self.df.sdjj.shift(1).rolling(window=n, center=False).min()
+
 
 if __name__ == '__main__':
 
     #g = General('rb')
     #g.foo()
     gi = GeneralIndex('rb')
-    print gi.df['sdjj']
+    #print gi.df['sdjj']
+    #gi.get_ma5()
+    #print gi.df.loc[:, ['c','ma5']]
+    #print gi.df.describe()
     #gi.sdjj
+    #gi.get_ma(5,10,20)
+    
+    #print gi.df.loc[:,['c','ma5', 'ma10', 'ma20']]
+    gi.get_nsdl(10)
+    print gi.df.loc[:, ['date','sdjj','nsdl']]
+    #print gi.df.describe()
--- restart/sdjj.py
+++ restart/sdjj.py
@@ -6,23 +6,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 from general_index import General, GeneralIndex
 
-'''
-四点均价  开收盘价，最高价，最低价
-'''
 
 class Sdjj(GeneralIndex):
     def __init__(self, daima):
         super(Sdjj, self).__init__(daima)
-        self.sdjj()
+        self.get_ma(5,10,20)
+
 
     def foo(self):
         print self.df['o']
         pass
 
+    def tupo_sdhsdl(self, n=10):
+        '''四点均线突破前n天最高开多，突破前n天最低开空
+        暂时还想不出移动止损怎么写，先写不带移动止损的，用信号平仓的策略
+        '''
+        self.get_nsdl(n)
+        self.get_nsdh(n)
+        #self.df['tupo_sdh'] = self.df.sdjj.shift(1) < self.df.nsdh.shift(1) & self.df.sdjj > self.df.nsdh 
+        self.df['tmp1'] = self.df.sdjj.shift(1) < self.df.nsdh.shift(1)
+        self.df['tmp2'] = self.df.sdjj > self.df.nsdh
+        self.df['tupo_sdh'] = self.df.tmp1 & self.df.tmp2  #今天的四点均价突破前n天的四点均价高点
+        self.df['bksk'] = np.where(self.df['tupo_sdh'], 'bk' , None)
+        #self.df.loc[: ,'bkcnt'] = 0
+        #self.df['bkcnt'] = np.where(self.df['tupo_sdh'], self.df.bkcnt.shift(1)+1 , self.df.bkcnt.shift(2))
+
+        self.df['tmp1'] = self.df.sdjj.shift(1) > self.df.nsdl.shift(1)
+        self.df['tmp2'] = self.df.sdjj < self.df.nsdl
+        self.df['tupo_sdl'] = self.df.tmp1 & self.df.tmp2
+        # bk表示买开仓或买平仓，sk相反
+        self.df['bksk'] = np.where(self.df['tupo_sdl'], 'sk' , self.df['bksk'])
+        for i, bksk in enumerate(self.df.bksk):
+            bkcnt = 0 # 每出一次开仓信号，就开一手，共几手的计数。一旦相反信号出来全平仓
+            if bksk == 'bk':
+                idx = self.df.index[i]
+               
+                bkprice = self.df.loc[idx, 'sdjj'] # 买开仓的价位
+                #print bkprice
+                bkcnt += 1
 
 if __name__ == '__main__':
     s = Sdjj('rb')
     #s.foo()
-    print s.df['sdjj']
+    
+    s.tupo_sdhsdl()
+    #print s.df.index
+    #print s.df.loc[:, ['date','sdjj', 'tupo_sdl', 'tupo_sdh']]
+    #s.df.to_csv('tmp.csv')  
     #df = pd.read_csv('../data/%s.xls' % 'RBL9')
     #print df
