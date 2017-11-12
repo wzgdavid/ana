034405f4876b67d672c44cbaa640fce2b5c4404c
wzgdavid <wzgdavid@sina.com>
2017-11-12 15:31:01 +0800

    初步跑出来，结果不如人意啊，休息会儿

--- DKX/a.py
+++ DKX/a.py
@@ -45,13 +45,95 @@ def get_nll(df, n):
     return df
 
 
-
-
-def run(zijin=100000, f=0.01):
-    pass
-
 df = get_DKX(df)
 df = get_nhh(df, 2)
 df = get_nll(df, 2)
+# 趋势判断，DKXb方向，1 向上   0向下  当天参照前两天
+df['DKXb方向'] = np.where(df.b.shift(1)<df.b.shift(2), 1, 0) 
+# 开仓条件
+df['高于前两天高点'] = np.where(df.h > df.nhh, 1, None)   # 看当天 
+df['低于前两天低点'] = np.where(df.l < df.nll, 1, None)
+# 开仓  bk开多  sk开空
+df['开仓'] = np.where((df['高于前两天高点'] == 1) & (df['DKXb方向']==1), 'bk', None)
+df['开仓'] = np.where((df['低于前两天低点'] == 1) & (df['DKXb方向']==0), 'sk', df['开仓'] )
+# 平仓 趋势反转 'bp' 平多  'sp' 平空
+df['平仓'] = np.where((df.DKXb方向.shift(2) == 1) & (df.DKXb方向.shift(1) == 0), 'bp', None)
+df['平仓'] = np.where((df.DKXb方向.shift(2) == 0) & (df.DKXb方向.shift(1) == 1), 'sp', df['平仓'])
 
-print(df.head(50))
+#平仓的同时不反向开仓
+df['开仓'] = np.where(df['平仓'].isnull(), df['开仓'], None)
+
+# 多空持仓总手数
+df['bk总手数'] = 0
+df['sk总手数'] = 0
+
+df['开仓手数'] = 1  # 目前简单处理，固定手数  后面加入资金管理
+
+df['未持仓金额'] = 0
+df.未持仓金额.iloc[0] = 200000  # 初始化
+df['b持仓金额'] = 0
+df['s持仓金额'] = 0
+bk_cnt = 0 # 多仓开仓总数量
+sk_cnt = 0 # 开空仓开仓总数量
+
+'''
+删选出一部分试验 ，否则太慢， 注意以后要去掉
+'''
+#df = df.head(200) # 删选出一部分试验 ，否则太慢， 注意以后要去掉
+
+点数 = 10 # 螺纹一手10吨
+保证金 = 0.1 # 保证金10%
+db = 点数*保证金
+for idx in range(df.shape[0]): # 
+    row = df.iloc[idx]
+    print(idx)
+    if row.开仓 == 'bk':
+        df.bk总手数.iloc[idx] = df.bk总手数.iloc[idx-1] + row.开仓手数
+        #df.b持仓金额.iloc[idx] += row.开仓手数 * row.c * 点数
+    else:
+        df.bk总手数.iloc[idx] = df.bk总手数.iloc[idx-1]
+        #df.b持仓金额.iloc[idx] += row.开仓手数 * row.c * 点数
+
+    if row.开仓 == 'sk':
+        df.sk总手数.iloc[idx] = df.sk总手数.iloc[idx-1] + row.开仓手数
+        #df.s持仓金额.iloc[idx] += row.开仓手数 * row.c * 点数
+    else:
+        df.sk总手数.iloc[idx] = df.sk总手数.iloc[idx-1]
+        #df.s持仓金额.iloc[idx] += row.开仓手数 * row.c * 点数
+
+    if row.平仓 == 'bp': 
+        #df.b持仓金额.iloc[idx] = 0
+        df.bk总手数.iloc[idx] = 0
+    elif row.平仓 == 'sp':
+        df.sk总手数.iloc[idx] = 0
+
+df['b持仓金额'] = df.bk总手数 * df.c * db
+df['s持仓金额'] = df.sk总手数 * df.c * db
+#df['总持仓金额'] = df.b持仓金额 + df.s持仓金额
+# 计算未持仓金额
+for idx in range(df.shape[0]): 
+    row = df.iloc[idx]
+    if idx == 0:
+        continue
+    print(idx)
+    df['未持仓金额'].iloc[idx] = df['未持仓金额'].iloc[idx-1]
+    if row.平仓 == 'bp':
+        df['未持仓金额'].iloc[idx] = df['未持仓金额'].iloc[idx-1] + df['b持仓金额'].iloc[idx-1]
+    if row.平仓 == 'sp':
+        df['未持仓金额'].iloc[idx] = df['未持仓金额'].iloc[idx-1] + df['s持仓金额'].iloc[idx-1]
+    if row.开仓 == 'bk':
+        df['未持仓金额'].iloc[idx] = df['未持仓金额'].iloc[idx-1] - row.开仓手数 * row.c
+    if row.开仓 == 'sk':
+        df['未持仓金额'].iloc[idx] = df['未持仓金额'].iloc[idx-1] - row.开仓手数 * row.c
+
+
+
+    
+    
+
+df['总金额'] = df.未持仓金额 + df['b持仓金额'] + df['s持仓金额']
+
+#df.bk总手数 = np.where(df.开仓=='bk', df.bk总手数.shift(1)+df.开仓手数, df.bk总手数.shift(1))
+#df.bk总手数 = df.bk总手数.shift(1)+df.开仓手数
+
+df.to_csv('tmp_dkx向下.csv')
