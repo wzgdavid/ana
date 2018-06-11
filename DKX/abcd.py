ed6d809912fa23e3231f85acc1b172b2de21c333
wzgdavid <wzgdavid@sina.com>
2018-06-11 20:47:55 +0800

    结果

--- DKX/abcd.py
+++ DKX/abcd.py
@@ -10,24 +10,23 @@ import seaborn as sns
 from common import *#get_DKX, get_nhh, get_nll, get_ma, avg,get_nhhzs,get_nllzs,get_atr
 
 def foo1():
-    pinzhong = 'pp'
+    pinzhong = 'y'
     plt.rcParams['font.sans-serif'] = ['SimHei']
     df = pd.read_csv(r'..\data\{}.csv'.format(pinzhong))
     df = get_ma(df, 20)
-    #df['condition'] = np.where(df.c.shift(1)>df.ma.shift(1), 1, None) 
+    #df['condition'] = np.where(df.c.shift(1)<df.ma.shift(1), 1, None) 
     
     df = df.dropna()
     df['收盘的波幅'] = df.c/df.c.shift(1)
     df['高价的波幅'] = df.h/df.c.shift(1)-1
     df['低价的波幅'] = 1-df.l/df.c.shift(1)
-    df['高到收'] = df.h/df.c
-    df['低到收'] = df.c/df.l
-    print(df['收盘的波幅'].std())
-    print(df['高价的波幅'].std())
-    print(df['低价的波幅'].std())
-    print(df.describe()[['收盘的波幅','高价的波幅','低价的波幅','高到收','低到收']])
+
+
+    print(df.describe()[['收盘的波幅','高价的波幅','低价的波幅']])
     df.to_csv('tmp.csv')
 foo1()
+
+
 
 '''
 ==============================================================================
